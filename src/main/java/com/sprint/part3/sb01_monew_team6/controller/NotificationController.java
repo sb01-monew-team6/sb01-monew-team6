8f84c754f496c027f480001feea71ffd0618f63e
FrogBaek <rnsdls1996@gmail.com>
2025-04-22 17:32:49 +0900

    test: notification 컨트롤러 정상 응답 - green

--- src/main/java/com/sprint/part3/sb01_monew_team6/controller/NotificationController.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/controller/NotificationController.java
@@ -1,26 +1,48 @@
 package com.sprint.part3.sb01_monew_team6.controller;
 
+import static org.springframework.data.domain.Sort.Direction.*;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sprint.part3.sb01_monew_team6.dto.PageResponse;
+import com.sprint.part3.sb01_monew_team6.dto.notification.NotificationDto;
+import com.sprint.part3.sb01_monew_team6.service.NotificationService;
 import com.sprint.part3.sb01_monew_team6.validation.group.NotificationValidationGroup;
 
 import jakarta.validation.constraints.Min;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/notifications")
 @Validated(NotificationValidationGroup.class)
+@RequiredArgsConstructor
 public class NotificationController {
 
+	@Autowired
+	private final NotificationService notificationService;
+
 	@GetMapping
-	public String findAllByUserId(
-		@RequestHeader("Monew-Request-User-Id")
-		@Min(value = 1, groups = NotificationValidationGroup.class) Long userId
+	public ResponseEntity<PageResponse<NotificationDto>> findAllByUserId(
+		@RequestHeader("Monew-Request-User-Id") @Min(value = 1, groups = NotificationValidationGroup.class) Long userId,
+		@RequestParam(required = false) Instant cursor,
+		@PageableDefault(
+			size = 50,
+			sort = "createdAt",
+			direction = DESC
+		) Pageable pageable
 	) {
 
-		return "";
+		return ResponseEntity.ok(notificationService.findAllByUserId(userId, cursor, pageable));
 	}
 }
