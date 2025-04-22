c5273d821674f4be5ae9109bd8d0b4d29df130c5
FrogBaek <rnsdls1996@gmail.com>
2025-04-22 16:17:19 +0900

    test: notification 컨트롤러 정상 응답 - red

--- src/test/java/com/sprint/part3/sb01_monew_team6/controller/NotificationControllerTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/controller/NotificationControllerTest.java
@@ -2,6 +2,7 @@ package com.sprint.part3.sb01_monew_team6.controller;
 
 import static com.sprint.part3.sb01_monew_team6.exception.ErrorCode.*;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,11 +22,13 @@ import com.sprint.part3.sb01_monew_team6.exception.GlobalExceptionHandler;
 @Import(GlobalExceptionHandler.class)
 class NotificationControllerTest {
 
+	private final NotificationService notificationService;
+
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
-	@DisplayName("findAllByUserId 호출 시 userId 가 0 이하면 NotificationException 발생")
+	@DisplayName("findAllByUserId 호출 시 userId 가 0 이하면 NotificationInvalidException 에러 반환")
 	public void respondErrorResponseWhenUserIdIsNullWhileFindAllByUserId() throws Exception {
 
 		//given & when
@@ -37,6 +40,26 @@ class NotificationControllerTest {
 		//then
 		ErrorCode notificationInvalidException = NOTIFICATION_INVALID_EXCEPTION;
 
+		perform.andExpect(jsonPath("$.status").value(equalTo(BAD_REQUEST.value())))
+			.andExpect(jsonPath("$.code").value(equalTo(notificationInvalidException.toString())))
+			.andExpect(jsonPath("$.message").value(equalTo(notificationInvalidException.getMessage())));
+	}
+
+	@Test
+	@DisplayName("findAllByUserId 정상 호출 시 정상 페이지네이션 반환")
+	public void respondPageResponseWhenFindAllByUserIdSuccessfully() throws Exception {
+
+		//given
+		NotificationDto notificationDto = new NotificationDto();
+		when(notificationService.findAllByUserId()).thenReturn(notificationDto);
+
+		//when
+		ResultActions perform = mockMvc.perform(
+			MockMvcRequestBuilders.get("/api/v1/notifications")
+				.header("Monew-Request-User-Id", 1L)
+		);
+
+		//then
 		perform.andExpect(jsonPath("$.status").value(equalTo(BAD_REQUEST.value())))
 			.andExpect(jsonPath("$.code").value(equalTo(notificationInvalidException.toString())))
 			.andExpect(jsonPath("$.message").value(equalTo(notificationInvalidException.getMessage())));
