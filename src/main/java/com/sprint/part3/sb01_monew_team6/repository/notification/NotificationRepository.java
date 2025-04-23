26f1baecc72da1aeb73205b717669c01a506baa4
FrogBaek <rnsdls1996@gmail.com>
2025-04-23 09:27:59 +0900

    test: notification 레포지토리 findAllByUserId 정상 호출 시 정상 slice 반환 - green

--- src/main/java/com/sprint/part3/sb01_monew_team6/repository/notification/NotificationRepository.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/repository/notification/NotificationRepository.java
@@ -5,9 +5,23 @@ import java.time.Instant;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sprint.part3.sb01_monew_team6.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-	Slice<Notification> findAllByUserId(Long userId, Instant createdAt, Pageable pageable);
+
+	@Query(
+		value = """
+			SELECT n FROM Notification n
+			WHERE (n.user.id = :userId)
+				AND (n.confirmed = false)
+				AND	(n.createdAt < :createdAt)
+			"""
+	)
+	Slice<Notification> findAllByUserId(
+		Long userId,
+		Instant createdAt,
+		Pageable pageable
+	);
 }
