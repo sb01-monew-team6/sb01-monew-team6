42fdc340cba21095ed8ad218b6a6cf95a28252ee
FrogBaek <rnsdls1996@gmail.com>
2025-04-23 21:15:18 +0900

    test: notification 이벤트 createNotificationByEventHandler 가 알림 생성 로직을 호출한다 - green

--- src/main/java/com/sprint/part3/sb01_monew_team6/handler/NotificationEventHandler.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/handler/NotificationEventHandler.java
@@ -1,5 +1,25 @@
 package com.sprint.part3.sb01_monew_team6.handler;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.sprint.part3.sb01_monew_team6.event.NotificationCreateEvent;
+import com.sprint.part3.sb01_monew_team6.service.NotificationService;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class NotificationEventHandler {
+
+	private final NotificationService notificationService;
+
+	@Async
+	@Transactional
+	@EventListener
+	public void handle(NotificationCreateEvent event) {
+		notificationService.createFromEvent(event);
+	}
 }
