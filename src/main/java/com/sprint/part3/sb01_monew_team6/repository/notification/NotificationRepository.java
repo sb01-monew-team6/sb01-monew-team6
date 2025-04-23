3a3da0267e45097b4a0866d659eac052e2684028
FrogBaek <rnsdls1996@gmail.com>
2025-04-23 10:47:05 +0900

    test: notification 레포지토리 countByUserIdAndConfirmedFalse 정상 호출 시 정상 count 반환 - refactor

--- src/main/java/com/sprint/part3/sb01_monew_team6/repository/notification/NotificationRepository.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/repository/notification/NotificationRepository.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.sprint.part3.sb01_monew_team6.entity.Notification;
 
@@ -13,5 +14,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 	@Override
 	Slice<Notification> findAllByUserId(Long userId, Instant createdAt, Pageable pageable);
 
+	@Query(value = """
+			SELECT COUNT(n) FROM Notification n
+			WHERE (n.user.id = :userId)
+				AND (n.confirmed = false)
+		""")
 	long countByUserIdAndConfirmedFalse(Long userId);
 }
