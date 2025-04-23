d6134c91eca6af6ce8dbfa81aa1adf7317389798
FrogBaek <rnsdls1996@gmail.com>
2025-04-23 15:04:05 +0900

    test: notification 레포지토리 updateByUserId 정상 호출 시 정상 값 반환 - green

--- src/main/java/com/sprint/part3/sb01_monew_team6/repository/notification/NotificationRepository.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/repository/notification/NotificationRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.sprint.part3.sb01_monew_team6.entity.Notification;
 
@@ -31,5 +30,12 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 		""")
 	void updateAllByUserId(Long userId);
 
+	@Modifying(clearAutomatically = true, flushAutomatically = true)
+	@Query(value = """
+			UPDATE Notification n
+			SET n.confirmed = true
+			WHERE (n.user.id = :userId)
+				AND (n.id = :notificationId)
+		""")
 	void updateByUserId(Long userId, Long notificationId);
 }
