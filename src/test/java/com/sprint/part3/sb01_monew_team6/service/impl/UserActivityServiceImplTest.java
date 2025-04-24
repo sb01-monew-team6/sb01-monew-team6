82b5ed324d7e2d45ae012ee6df53002a1f6c94d0
FrogBaek <rnsdls1996@gmail.com>
2025-04-24 20:35:00 +0900

    test: userActivity 서비스 addSubscriptionFromEvent 호출 시 user 가 존재하지 않는다면 UserActivityDomainException 발생 - red

--- src/test/java/com/sprint/part3/sb01_monew_team6/service/impl/UserActivityServiceImplTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/service/impl/UserActivityServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.sprint.part3.sb01_monew_team6.service.impl;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
@@ -15,6 +16,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.sprint.part3.sb01_monew_team6.dto.user_activity.SubscriptionHistoryDto;
 import com.sprint.part3.sb01_monew_team6.entity.User;
 import com.sprint.part3.sb01_monew_team6.entity.UserActivity;
+import com.sprint.part3.sb01_monew_team6.entity.enums.ResourceType;
+import com.sprint.part3.sb01_monew_team6.event.NotificationCreateEvent;
+import com.sprint.part3.sb01_monew_team6.exception.notification.NotificationDomainException;
 import com.sprint.part3.sb01_monew_team6.mapper.SubscriptionHistoryMapper;
 import com.sprint.part3.sb01_monew_team6.repository.UserRepository;
 import com.sprint.part3.sb01_monew_team6.repository.user_activity.UserActivityRepository;
@@ -61,5 +65,25 @@ class UserActivityServiceImplTest {
 		//then
 		verify(userActivityRepository, times(1)).addSubscription(anyLong(),
 			any(UserActivity.SubscriptionHistory.class));
+	}
+
+	@Test
+	@DisplayName("addSubscriptionFromEvent 호출 시 user 가 존재하지 않는다면 UserActivityDomainException 발생")
+	public void throwUserActivityDomainExceptionWhenUserNonExistWhileAddSubscriptionFromEvent() throws Exception {
+		//given
+		Long userId = 1L;
+		SubscriptionHistoryDto historyDto = new SubscriptionHistoryDto(
+			1L,
+			"interestName",
+			List.of("k1", "k2"),
+			3L
+		);
+		when(userRepository.findById(eq(userId))).thenReturn(Optional.empty());
+
+		//when & then
+		assertThatThrownBy(() ->
+			userActivityRepository.addSubscriptionFromEvent(userId, historyDto)
+		).isInstanceOf(UserActivityDomainException.class);
+
 	}
 }
