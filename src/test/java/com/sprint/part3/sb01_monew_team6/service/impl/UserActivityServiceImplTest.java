3c39666bdeea0f05ab2d5c88d9079bbda6e278c6
FrogBaek <rnsdls1996@gmail.com>
2025-04-24 22:36:43 +0900

    userActivity 서비스 addCommentLikeFromEvent 정상 호출 시 정상적으로 레포지토리가 호출된다 - red

--- src/test/java/com/sprint/part3/sb01_monew_team6/service/impl/UserActivityServiceImplTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/service/impl/UserActivityServiceImplTest.java
@@ -3,6 +3,7 @@ package com.sprint.part3.sb01_monew_team6.service.impl;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.sprint.part3.sb01_monew_team6.dto.user_activity.CommentLikeHistoryDto;
 import com.sprint.part3.sb01_monew_team6.dto.user_activity.SubscriptionHistoryDto;
 import com.sprint.part3.sb01_monew_team6.entity.User;
 import com.sprint.part3.sb01_monew_team6.entity.UserActivity;
@@ -29,6 +31,8 @@ class UserActivityServiceImplTest {
 
 	@Mock
 	private SubscriptionHistoryMapper subscriptionHistoryMapper;
+	@Mock
+	private CommentLikeHistoryMapper commentLikeHistoryMapper;
 	@Mock
 	private UserActivityRepository userActivityRepository;
 	@Mock
@@ -86,5 +90,44 @@ class UserActivityServiceImplTest {
 			userActivityService.addSubscriptionFromEvent(userId, historyDto)
 		).isInstanceOf(UserActivityDomainException.class);
 
+	}
+
+	@Test
+	@DisplayName("addCommentLikeFromEvent 정상 호출 시 정상적으로 레포지토리가 호출된다")
+	public void addCommentLikeFromEvent() throws Exception {
+		//given
+		Long userId = 1L;
+		UserActivity.CommentLikeHistory history = new UserActivity.CommentLikeHistory(
+			1L,
+			1L,
+			"interestName",
+			1L,
+			"nickName",
+			"content",
+			3L,
+			Instant.now()
+		);
+		CommentLikeHistoryDto historyDto = new CommentLikeHistoryDto(
+			1L,
+			1L,
+			"interestName",
+			1L,
+			"nickName",
+			"content",
+			3L,
+			Instant.now()
+		);
+
+		when(commentLikeHistoryMapper.fromDto(any(CommentLikeHistoryDto.class))).thenReturn(
+			history);
+		when(userRepository.findById(anyLong())).thenReturn(Optional.of(new User()));
+		doNothing().when(userActivityRepository).addCommentLike(anyLong(), any(UserActivity.CommentLikeHistory.class));
+
+		//when
+		userActivityService.addCommentLikeFromEvent(userId, historyDto);
+
+		//then
+		verify(userActivityRepository, times(1)).addCommentLike(anyLong(),
+			any(UserActivity.CommentLikeHistory.class));
 	}
 }
