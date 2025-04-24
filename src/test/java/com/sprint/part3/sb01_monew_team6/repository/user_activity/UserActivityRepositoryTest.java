5feb4efacc144dc712952268523952ddb3c2a87c
FrogBaek <rnsdls1996@gmail.com>
2025-04-24 13:37:31 +0900

    test: userActivity 레포지토리 addSubscription 정상 호출 시 정상적으로 몽고 db 에 적재된다 - red

--- src/test/java/com/sprint/part3/sb01_monew_team6/repository/user_activity/UserActivityRepositoryTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/repository/user_activity/UserActivityRepositoryTest.java
@@ -1,9 +1,8 @@
-package com.sprint.part3.sb01_monew_team6.repository;
+package com.sprint.part3.sb01_monew_team6.repository.user_activity;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.sprint.part3.sb01_monew_team6.config.TestDataMongoConfig;
 import com.sprint.part3.sb01_monew_team6.entity.UserActivity;
+import com.sprint.part3.sb01_monew_team6.repository.UserActivityRepository;
 
 @DataMongoTest
 @ActiveProfiles("test")
@@ -55,5 +55,44 @@ class UserActivityRepositoryTest {
 		assertThat(found.get().getSubscriptions().get(0).getInterestName()).isEqualTo("AI");
 		assertThat(found.get().getSubscriptions().get(0).getInterestKeywords()).containsExactly("ChatGPT", "머신러닝");
 		assertThat(found.get().getSubscriptions().get(0).getId()).isEqualTo(subscription.getId());
+	}
+
+	@Test
+	@DisplayName("addSubscription 정상 호출 시 정상적으로 몽고 db 에 적재된다")
+	public void addSubscriptionSuccessfully() throws Exception {
+	    //given
+		Long userId = 1L;
+
+		UserActivity userActivity = UserActivity.builder()
+			.userId(userId)
+			.email("email@google.com")
+			.nickName("구글러")
+			.build();
+
+		userActivityRepository.save(userActivity);
+
+	    //when
+		for (int i = 0; i < 12; ++i) {
+			UserActivity.SubscriptionHistory subscription = UserActivity.SubscriptionHistory.builder()
+				.interestId(10L + i)
+				.interestName("AI" + i)
+				.interestKeywords(List.of("ChatGPT" + i, "머신러닝" + i))
+				.interestSubscriberCount(10L + i)
+				.build();
+
+			userActivityRepository.addSubscription(userId, subscription);
+		}
+
+		Optional<UserActivity> found = userActivityRepository.findById(userActivity.getId());
+
+		//then
+		assertThat(found).isPresent();
+		assertThat(found.get().getId()).isEqualTo(userActivity.getId());
+		assertThat(found.get().getUserId()).isEqualTo(userId);
+		assertThat(found.get().getEmail()).isEqualTo("email@google.com");
+		assertThat(found.get().getNickName()).isEqualTo("구글러");
+		assertThat(found.get().getSubscriptions()).hasSize(10);
+		assertThat(found.get().getSubscriptions().get(0).getInterestName()).isEqualTo("AI2");
+		assertThat(found.get().getSubscriptions().get(0).getInterestName()).isEqualTo("AI11");
 	}
 }
