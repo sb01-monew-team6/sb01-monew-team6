f93745d820dfb369068fc34aee4794dccbe7e9b6
FrogBaek <rnsdls1996@gmail.com>
2025-04-25 10:30:30 +0900

    test: commentLikeHistory 매퍼 fromDto 정상 호출 시 dto 가 엔티티 로 정상 변환  - red, green

---
+++ src/test/java/com/sprint/part3/sb01_monew_team6/mapper/CommentLikeHistoryMapperTest.java
@@ -0,0 +1,48 @@
+package com.sprint.part3.sb01_monew_team6.mapper;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Instant;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import com.sprint.part3.sb01_monew_team6.dto.user_activity.CommentLikeHistoryDto;
+import com.sprint.part3.sb01_monew_team6.dto.user_activity.SubscriptionHistoryDto;
+import com.sprint.part3.sb01_monew_team6.entity.UserActivity;
+
+class CommentLikeHistoryMapperTest {
+
+	private final CommentLikeHistoryMapper mapper = Mappers.getMapper(CommentLikeHistoryMapper.class);
+
+	@Test
+	@DisplayName("fromDto 정상 호출 시 dto 가 엔티티 로 정상 변환")
+	public void returnEntityWhenFromDtoSuccessfullyCalled() throws Exception {
+		//given
+		CommentLikeHistoryDto dto = new CommentLikeHistoryDto(
+			1L,
+			1L,
+			"title",
+			1L,
+			"nickname",
+			"content",
+			1L,
+			Instant.now()
+		);
+
+		//when
+		UserActivity.CommentLikeHistory entity = mapper.fromDto(dto);
+
+		//then
+		assertThat(entity.getCommentId()).isEqualTo(1L);
+		assertThat(entity.getArticleId()).isEqualTo(1L);
+		assertThat(entity.getArticleTitle()).isEqualTo("title");
+		assertThat(entity.getCommentUserId()).isEqualTo(1L);
+		assertThat(entity.getCommentUserNickname()).isEqualTo("nickname");
+		assertThat(entity.getCommentContent()).isEqualTo("content");
+		assertThat(entity.getCommentLikeCount()).isEqualTo(1L);
+	}
+}
