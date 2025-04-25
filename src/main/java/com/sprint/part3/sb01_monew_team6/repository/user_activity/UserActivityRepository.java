7db6682c631a02da5b4b2eecd8936cdfd3613430
FrogBaek <rnsdls1996@gmail.com>
2025-04-25 14:44:17 +0900

    test: userActivity 서비스 findByUserId 정상 호출 시 정상적으로 레포지토리가 호출된다 - green

--- src/main/java/com/sprint/part3/sb01_monew_team6/repository/user_activity/UserActivityRepository.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/repository/user_activity/UserActivityRepository.java
@@ -1,8 +1,12 @@
 package com.sprint.part3.sb01_monew_team6.repository.user_activity;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.sprint.part3.sb01_monew_team6.entity.UserActivity;
 
 public interface UserActivityRepository extends MongoRepository<UserActivity, String>, UserActivityRepositoryCustom {
+
+	Optional<UserActivity> findByUserId(Long userId);
 }
