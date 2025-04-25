ebe0e3d330ba4bb16505fa544838c41a4a17a7fd
Son DongHyuk <sond5510@naver.com>
2025-04-25 14:33:08 +0900

    refactor : SchedulingConfigTest , Add BatchConfig

--- src/main/java/com/sprint/part3/sb01_monew_team6/config/SchedulingConfig.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/config/SchedulingConfig.java
@@ -1,7 +1,15 @@
 package com.sprint.part3.sb01_monew_team6.config;
 
-import com.sprint.part3.sb01_monew_team6.service.NewsCollectionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -9,12 +17,29 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
+@Slf4j
 public class SchedulingConfig {
-  private final NewsCollectionService service;
   private static final String CRON = "0 0 0 * * *";
+  private final JobLauncher jobLauncher;
+  private final Job newsJob;
 
   @Scheduled(cron = CRON)
   public void collectNewsSchedule(){
-    service.collectAndSave();
+    // 실행마다 새로운 JobInstance 생성
+    JobParameters jobParameters = new JobParametersBuilder()
+        .addLong("time",System.currentTimeMillis())
+        .toJobParameters();
+
+    try{
+      jobLauncher.run(newsJob,jobParameters);
+    }catch (JobExecutionAlreadyRunningException
+            | JobRestartException
+            | JobInstanceAlreadyCompleteException
+            | JobParametersInvalidException e) {
+      log.error("배치 뉴스 수집 작업 실행 중 오류 발생", e);
+      throw new RuntimeException("배치 작업 실행 실패", e);
+    }
+
+    log.info("스케줄러에 의해 배치 '{}' 작업이 성공적으로 실행되었습니다", newsJob.getName());
   }
 }
--- src/main/java/com/sprint/part3/sb01_monew_team6/service/NewsCollectionService.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/service/NewsCollectionService.java
@@ -31,26 +31,55 @@ public class NewsCollectionService {
     if (interests.isEmpty()) {
       return;
     }
-
+    // 외부 뉴스 수집
     List<ExternalNewsItem> externalNewsItems = fetchExternalNews(interests);
+    // 필터링,엔티티 변환
     List<NewsArticle> toSave = filterAndPrepareNewsArticles(externalNewsItems, interests);
 
+    //저장
     if (!toSave.isEmpty()) {
       newsArticleRepository.saveAll(toSave);
     }
   }
 
+  //Batch Chunk - ItemReader
+  //관심사 기준으로 외부 뉴스 수집
+  public List<ExternalNewsItem> fetchCandidates(){
+    List<Interest> interests = interestRepository.findAll();
+    //관심사 x
+    if(interests.isEmpty()){
+      return List.of();
+    }
+    //관심사 o
+    return fetchExternalNews(interests);
+  }
+
+  //Batch Chunk - ItemWrtier
+  //NewsArticle 목록을 받아 DB에 중복 없이 저장
+  public void saveAll(List<NewsArticle> articles){
+    List<NewsArticle> batchArticles = new ArrayList<>();
+    for(NewsArticle article:articles){
+      if(!newsArticleRepository.existsBySourceUrl(article.getSourceUrl())){
+        batchArticles.add(article);
+      }
+    }
+    if(!batchArticles.isEmpty()){
+      newsArticleRepository.saveAll(batchArticles);
+    }
+  }
+
+  // 관심사 키워드 기반으로 Naver + RSS 에서 뉴스 아이템 수집
   private List<ExternalNewsItem> fetchExternalNews(List<Interest> interests) {
     List<ExternalNewsItem> items = new ArrayList<>();
 
-    // 네이버 뉴스 페칭
+    // 네이버 뉴스
     for (Interest interest : interests) {
       for (String keyword : interest.getKeyword()) {
         items.addAll(naver.fetchNews(keyword));
       }
     }
 
-    // RSS 뉴스 페칭
+    // RSS 뉴스
     for (RssNewsClient rssClient : rssClients) {
       items.addAll(rssClient.fetchNews());
     }
@@ -58,6 +87,7 @@ public class NewsCollectionService {
     return items;
   }
 
+  //
   private List<NewsArticle> filterAndPrepareNewsArticles(List<ExternalNewsItem> items, List<Interest> interests) {
     List<NewsArticle> toSave = new ArrayList<>();
     Set<String> seenUrls = new HashSet<>(); // 이미 처리한 URL 기록
