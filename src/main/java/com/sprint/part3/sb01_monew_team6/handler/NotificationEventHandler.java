f7f136a754817256d02a361a4dc359a0213ea966
FrogBaek <rnsdls1996@gmail.com>
2025-04-25 12:27:27 +0900

    refactor: notification 이벤트 핸들러 handleNotificationCreateEvent 가독성 고려 코드 개선

--- src/main/java/com/sprint/part3/sb01_monew_team6/handler/NotificationEventHandler.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/handler/NotificationEventHandler.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.sprint.part3.sb01_monew_team6.event.NotificationCreateEvent;
 import com.sprint.part3.sb01_monew_team6.exception.notification.NotificationDomainException;
+import com.sprint.part3.sb01_monew_team6.exception.user_activity.UserActivityDomainException;
 import com.sprint.part3.sb01_monew_team6.service.NotificationService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,29 +25,46 @@ public class NotificationEventHandler {
 	public void handleNotificationCreateEvent(NotificationCreateEvent event) {
 
 		validateEvent(event);
+		validateUserId(event.userId());
+		validateByResourceType(event);
 
 		notificationService.createFromEvent(event);
 	}
 
-	private static void validateEvent(NotificationCreateEvent event) {
-
-		if (Objects.isNull(event.userId()) || event.userId() <= 0) {
-			throw new NotificationDomainException("유저 id 가 유효하지 않습니다.",
-				Map.of("userId", String.valueOf(event.userId())));
-		}
-
+	private static void validateByResourceType(NotificationCreateEvent event) {
 		switch (event.resourceType()) {
 			case INTEREST:
-				if (Objects.isNull(event.resourceContent()) || event.resourceContent().isBlank()) {
-					throw new NotificationDomainException("리소스 내용이 유효하지 않습니다.",
-						Map.of("resourceContent", String.valueOf(event.resourceContent())));
-				}
-
-				if (Objects.isNull(event.articleCount()) || event.articleCount() <= 0) {
-					throw new NotificationDomainException("기사의 개수가 유효하지 않습니다.",
-						Map.of("articleCount", String.valueOf(event.articleCount())));
-				}
+				validateResourceContent(event.resourceContent());
+				validateArticleCount(event.articleCount());
 				break;
+		}
+	}
+
+	private static void validateArticleCount(Long articleCount) {
+		if (Objects.isNull(articleCount) || articleCount <= 0) {
+			throw new NotificationDomainException("기사의 개수가 유효하지 않습니다.",
+				Map.of("articleCount", String.valueOf(articleCount)));
+		}
+	}
+
+	private static void validateResourceContent(String resourceContent) {
+		if (Objects.isNull(resourceContent) || resourceContent.isBlank()) {
+			throw new NotificationDomainException("리소스 내용이 유효하지 않습니다.",
+				Map.of("resourceContent", String.valueOf(resourceContent)));
+		}
+	}
+
+	private static void validateUserId(Long userId) {
+		if (Objects.isNull(userId) || userId <= 0) {
+			throw new NotificationDomainException("유저 id 가 유효하지 않습니다.",
+				Map.of("userId", String.valueOf(userId)));
+		}
+	}
+
+	private static void validateEvent(NotificationCreateEvent event) {
+		if (Objects.isNull(event)) {
+			throw new UserActivityDomainException("이벤트가 null 일 수 없습니다.",
+				Map.of("event", String.valueOf(event)));
 		}
 	}
 }
