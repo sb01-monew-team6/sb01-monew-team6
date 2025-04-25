75db3fe5f7958f256835d4e48bbfc749a6aa9bcc
FrogBaek <rnsdls1996@gmail.com>
2025-04-25 10:36:20 +0900

    test: articleViewHistory 매퍼 fromDto 정상 호출 시 dto 가 엔티티 로 정상 변환  - red, green

---
+++ src/test/java/com/sprint/part3/sb01_monew_team6/mapper/ArticleViewHistoryMapperTest.java
@@ -0,0 +1,48 @@
+package com.sprint.part3.sb01_monew_team6.mapper;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import com.sprint.part3.sb01_monew_team6.dto.user_activity.ArticleViewHistoryDto;
+import com.sprint.part3.sb01_monew_team6.entity.UserActivity;
+
+class ArticleViewHistoryMapperTest {
+
+	private final ArticleViewHistoryMapper mapper = Mappers.getMapper(ArticleViewHistoryMapper.class);
+
+	@Test
+	@DisplayName("fromDto 정상 호출 시 dto 가 엔티티 로 정상 변환")
+	public void returnEntityWhenFromDtoSuccessfullyCalled() throws Exception {
+		//given
+		ArticleViewHistoryDto dto = new ArticleViewHistoryDto(
+			1L,
+			1L,
+			"src",
+			"url",
+			"title",
+			LocalDateTime.now(),
+			"summary",
+			1L,
+			1L
+		);
+
+		//when
+		UserActivity.ArticleViewHistory entity = mapper.fromDto(dto);
+
+		//then
+		assertThat(entity.getViewedBy()).isEqualTo(1L);
+		assertThat(entity.getArticleId()).isEqualTo(1L);
+		assertThat(entity.getSource()).isEqualTo("src");
+		assertThat(entity.getSourceUrl()).isEqualTo("url");
+		assertThat(entity.getArticleTitle()).isEqualTo("title");
+		assertThat(entity.getArticleSummary()).isEqualTo("summary");
+		assertThat(entity.getArticleCommentCount()).isEqualTo(1L);
+		assertThat(entity.getArticleViewCount()).isEqualTo(1L);
+	}
+}
