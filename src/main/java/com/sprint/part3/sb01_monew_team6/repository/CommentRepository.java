16af0bf7e0152b7925edeb0385dcc212092d63c3
Son DongHyuk <sond5510@naver.com>
2025-04-28 13:33:56 +0900

    refactor : CommentRepository

--- src/main/java/com/sprint/part3/sb01_monew_team6/repository/CommentRepository.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/repository/CommentRepository.java
@@ -2,7 +2,9 @@ package com.sprint.part3.sb01_monew_team6.repository;
 
 import com.sprint.part3.sb01_monew_team6.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
   //특정 articleId 의 댓글 수 조회
