c5c1bd54d1ebc96c23449195d40f302a31c77a06
Son DongHyuk <sond5510@naver.com>
2025-04-28 13:06:39 +0900

    refactor : ArticleViewRepository

--- src/main/java/com/sprint/part3/sb01_monew_team6/repository/ArticleViewRepository.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/repository/ArticleViewRepository.java
@@ -2,7 +2,9 @@ package com.sprint.part3.sb01_monew_team6.repository;
 
 import com.sprint.part3.sb01_monew_team6.entity.ArticleView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleViewRepository extends JpaRepository<ArticleView,Long> {
 
   //articleId 로 ArticleView 수를 조회
