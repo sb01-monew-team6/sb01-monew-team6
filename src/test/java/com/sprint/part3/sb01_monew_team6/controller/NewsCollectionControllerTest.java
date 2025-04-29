29d48c978e4a2d5a773c196812f84765e06ab278
Son DongHyuk <sond5510@naver.com>
2025-04-29 17:24:23 +0900

    refactor : ArticleViewController

--- src/test/java/com/sprint/part3/sb01_monew_team6/controller/NewsCollectionControllerTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/controller/NewsCollectionControllerTest.java
@@ -13,18 +13,16 @@ import com.sprint.part3.sb01_monew_team6.service.NewsCollectionService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(ArticleViewController.class)
-@ActiveProfiles("test")
+@WebMvcTest(NewsCollectionController.class)
 @Import(GlobalExceptionHandler.class)
-@AutoConfigureMockMvc(addFilters = false)
+@WithMockUser
 public class NewsCollectionControllerTest {
   @Autowired
   MockMvc mvc;
