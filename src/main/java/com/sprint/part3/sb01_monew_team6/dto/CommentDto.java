04ddb78847ee30dab62baa657a6924312a7699f4
mingi96 <minnet1030@naver.com>
2025-04-29 17:20:38 +0900

    test: 댓글 등록 서비스 Green 구현 및 테스트 통과

--- src/main/java/com/sprint/part3/sb01_monew_team6/dto/CommentDto.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/dto/CommentDto.java
@@ -1,7 +1,10 @@
 package com.sprint.part3.sb01_monew_team6.dto;
 
+import lombok.Builder;
+
 import java.time.Instant;
 
+@Builder
 public record CommentDto(
         Long id,
         Long articleId,
