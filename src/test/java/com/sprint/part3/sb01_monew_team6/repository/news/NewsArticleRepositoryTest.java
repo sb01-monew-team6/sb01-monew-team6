baea91663a1cf19372e72371535ba52b05446753
Son DongHyuk <sond5510@naver.com>
2025-04-29 11:39:29 +0900

    refactor : test annotation

--- src/test/java/com/sprint/part3/sb01_monew_team6/repository/news/NewsArticleRepositoryTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/repository/news/NewsArticleRepositoryTest.java
@@ -1,18 +1,24 @@
  package com.sprint.part3.sb01_monew_team6.repository.news;
 
  import static org.assertj.core.api.Assertions.assertThat;
+ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
+ import com.sprint.part3.sb01_monew_team6.config.TestDataJpaConfig;
  import com.sprint.part3.sb01_monew_team6.entity.NewsArticle;
  import com.sprint.part3.sb01_monew_team6.repository.NewsArticleRepository;
  import java.time.Instant;
  import org.junit.jupiter.api.DisplayName;
  import org.junit.jupiter.api.Test;
  import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
  import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+ import org.springframework.context.annotation.Import;
  import org.springframework.test.context.ActiveProfiles;
 
  @DataJpaTest
  @ActiveProfiles("test")
+ @Import(TestDataJpaConfig.class)
+ @AutoConfigureTestDatabase(replace = NONE)
  public class NewsArticleRepositoryTest {
    @Autowired
    private NewsArticleRepository newsArticleRepository;
