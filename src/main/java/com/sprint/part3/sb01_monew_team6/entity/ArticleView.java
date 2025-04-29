241a0effd33ffba613e141ea776deb8f4b5fa3e5
Son DongHyuk <sond5510@naver.com>
2025-04-29 10:33:22 +0900

    fix : ArticleViewEntity 수정 및 관련된 테스트 주석

--- src/main/java/com/sprint/part3/sb01_monew_team6/entity/ArticleView.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/entity/ArticleView.java
@@ -1,9 +1,11 @@
 package com.sprint.part3.sb01_monew_team6.entity;
 
-import com.sprint.part3.sb01_monew_team6.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -12,7 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "article_view")
@@ -20,23 +21,19 @@ import org.hibernate.annotations.CreationTimestamp;
 @AllArgsConstructor
 @Getter
 @Setter
-public class ArticleView extends BaseUpdatableEntity {
+public class ArticleView{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "article_id", nullable = false)
   private NewsArticle article;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", nullable = false)
+  @JoinColumn(name = "user_id",    nullable = false)
   private User user;
 
-  @CreationTimestamp
-  @Column(nullable = false, updatable = false)
+  @Column(name = "article_view_date", nullable = false)
   private Instant articleViewDate;
-
-  //중복 조회 시 마지막으로 기사 본 시간으로 갱신
-  public ArticleView updateArticleViewDate() {
-    this.articleViewDate = Instant.now();
-    return this;
-  }
 }
