32091184b094f0d31c9c22c7cf7783e1a4f1d317
Son DongHyuk <sond5510@naver.com>
2025-04-29 14:07:25 +0900

    refactor : ArticleViewController, NewsCollectionController

--- src/test/java/com/sprint/part3/sb01_monew_team6/controller/NewsCollectionControllerTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/controller/NewsCollectionControllerTest.java
@@ -1,25 +1,30 @@
 package com.sprint.part3.sb01_monew_team6.controller;
 
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.sprint.part3.sb01_monew_team6.exception.GlobalExceptionHandler;
 import com.sprint.part3.sb01_monew_team6.service.NewsCollectionService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(NewsCollectionController.class)
-@WithMockUser
+@WebMvcTest(ArticleViewController.class)
+@ActiveProfiles("test")
+@Import(GlobalExceptionHandler.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class NewsCollectionControllerTest {
   @Autowired
   MockMvc mvc;
