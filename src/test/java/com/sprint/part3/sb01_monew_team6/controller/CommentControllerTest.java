3804ca4fe900821ee21104fc7b9aedbd7a97a924
mingi96 <minnet1030@naver.com>
2025-04-29 15:19:04 +0900

    test: 댓글 목록 조회 정상 요청 테스트 작성 (Red)

--- src/test/java/com/sprint/part3/sb01_monew_team6/controller/CommentControllerTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/controller/CommentControllerTest.java
@@ -93,5 +93,19 @@ class CommentControllerTest {
                 .andExpect(status().isBadRequest());
     }
 
+    @DisplayName("댓글 목록 조회 성공 - 필수 파라미터를 모두 제공할 경우 200 OK 반환")
+    @Test
+    @WithMockUser
+    void getComments_withValidParams_shouldReturnOk() throws Exception {
+        mockMvc.perform(get("/api/comments")
+                        .param("orderBy", "createdAt")
+                        .param("direction", "DESC")
+                        .param("limit", "10")
+                        .header("Monew-Request-User-ID", "1")
+                        .with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+
 
 }
