9fb2165ea4a3fb4933a96b0ea2f3cb7145051d99
mingi96 <minnet1030@naver.com>
2025-04-29 15:27:13 +0900

    test: 댓글 목록 조회 데이터 반환 테스트 작성 (Red)

--- src/main/java/com/sprint/part3/sb01_monew_team6/service/CommentService.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/service/CommentService.java
@@ -3,7 +3,19 @@ package com.sprint.part3.sb01_monew_team6.service;
 import com.sprint.part3.sb01_monew_team6.dto.CommentDto;
 import com.sprint.part3.sb01_monew_team6.dto.CommentRegisterRequest;
 
+import java.util.List;
+
 public interface CommentService {
 
     CommentDto register(CommentRegisterRequest request);
+
+    List<CommentDto> findAll(
+            Long articleId,
+            String orderBy,
+            String direction,
+            String cursor,
+            String after,
+            Integer limit,
+            Long requestUserId
+    );
 }
--- src/main/java/com/sprint/part3/sb01_monew_team6/service/impl/CommentServiceImpl.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/service/impl/CommentServiceImpl.java
@@ -5,11 +5,27 @@ import com.sprint.part3.sb01_monew_team6.dto.CommentRegisterRequest;
 import com.sprint.part3.sb01_monew_team6.service.CommentService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CommentServiceImpl implements CommentService {
 
     @Override
     public CommentDto register(CommentRegisterRequest request) {
+        // 일단 임시로 아무거나 리턴 (테스트 통과용)
+        return null;
+    }
+
+    @Override
+    public List<CommentDto> findAll(
+            Long articleId,
+            String orderBy,
+            String direction,
+            String cursor,
+            String after,
+            Integer limit,
+            Long requestUserId
+    ) {
         // 일단 임시로 아무거나 리턴 (테스트 통과용)
         return null;
     }
