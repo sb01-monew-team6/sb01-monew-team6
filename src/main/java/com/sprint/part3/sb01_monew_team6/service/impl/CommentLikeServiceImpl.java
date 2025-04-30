7bb489dfa881bb5c4e480d405d4fcb98da44ecba
mingi96 <minnet1030@naver.com>
2025-04-30 17:36:23 +0900

    codecov

---
+++ src/main/java/com/sprint/part3/sb01_monew_team6/service/impl/CommentLikeServiceImpl.java
@@ -0,0 +1,6 @@
+package com.sprint.part3.sb01_monew_team6.service.impl;
+
+import com.sprint.part3.sb01_monew_team6.service.CommentLikeService;
+
+public class CommentLikeServiceImpl implements CommentLikeService {
+}
