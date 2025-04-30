d0897e30a2074a0fc6bcad17ca98249d9b43a05d
mingi96 <minnet1030@naver.com>
2025-04-30 11:41:45 +0900

    refactor: findAll 메소드 구현 및 테스트 성공

--- src/test/java/com/sprint/part3/sb01_monew_team6/service/CommentServiceTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/service/CommentServiceTest.java
@@ -15,7 +15,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.swing.text.html.Option;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
