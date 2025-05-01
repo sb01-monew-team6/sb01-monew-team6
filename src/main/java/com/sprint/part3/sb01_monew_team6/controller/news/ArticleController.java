3e3a06bf63723c0043c487bd161b4d3d3c1ae48b
Son DongHyuk <sond5510@naver.com>
2025-05-02 02:18:54 +0900

    test : ArticleControllerTest and ArticleController  restore (green)

--- src/main/java/com/sprint/part3/sb01_monew_team6/controller/news/ArticleController.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/controller/news/ArticleController.java
@@ -4,11 +4,15 @@ import static org.springframework.http.HttpStatus.OK;
 
 import com.sprint.part3.sb01_monew_team6.dto.PageResponse;
 import com.sprint.part3.sb01_monew_team6.dto.news.ArticleDto;
+import com.sprint.part3.sb01_monew_team6.dto.news.ArticleRestoreResultDto;
 import com.sprint.part3.sb01_monew_team6.dto.news.CursorPageRequestArticleDto;
 import com.sprint.part3.sb01_monew_team6.service.news.ArticleService;
+import java.io.IOException;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,5 +60,23 @@ public class ArticleController {
     PageResponse<ArticleDto> article = articleService.searchArticles(request);
     return ResponseEntity.status(OK)
         .body(article);
+  }
+
+  //백업 복구
+  @GetMapping("/restore")
+  public List<ArticleRestoreResultDto> restore(
+      @RequestParam("from")
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+      LocalDateTime from,
+
+      @RequestParam("to")
+      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+      LocalDateTime to
+  ) throws IOException {
+    // LocalDateTime → LocalDate 로 변환
+    return articleService.restore(
+        from.toLocalDate(),
+        to.toLocalDate()
+    );
   }
 }
