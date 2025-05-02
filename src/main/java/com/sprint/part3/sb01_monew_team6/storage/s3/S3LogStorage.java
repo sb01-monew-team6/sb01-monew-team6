85a4f4639307b41ff8b008ef03a886de9ae486f0
FrogBaek <rnsdls1996@gmail.com>
2025-05-02 13:00:25 +0900

    chore: test 커버리지 통과를 위해 테스트 하기 좋은 코드로 리팩토링

--- src/main/java/com/sprint/part3/sb01_monew_team6/storage/s3/S3LogStorage.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/storage/s3/S3LogStorage.java
@@ -1,16 +1,12 @@
 package com.sprint.part3.sb01_monew_team6.storage.s3;
 
-import java.io.IOException;
 import java.nio.file.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -22,19 +18,14 @@ public class S3LogStorage {
 
 	@Autowired
 	public S3LogStorage(
-		@Value("${storage.s3.access-key}") String accessKey,
-		@Value("${storage.s3.secret-key}") String secretKey,
-		@Value("${storage.s3.region}") String region,
-		@Value("${storage.s3.bucket}") String bucket) {
-		this(getS3Client(accessKey, secretKey, region), bucket);
-	}
-
-	public S3LogStorage(S3Client s3Client, String bucket) {
+		S3Client s3Client,
+		@Value("${storage.s3.bucket}") String bucket
+	) {
 		this.s3Client = s3Client;
 		this.bucket = bucket;
 	}
 
-	public void uploadZip(Path zipPath) throws IOException {
+	public void uploadZip(Path zipPath) {
 		String key = zipPath.getFileName().toString();
 
 		PutObjectRequest request = PutObjectRequest.builder()
@@ -43,18 +34,5 @@ public class S3LogStorage {
 			.build();
 
 		s3Client.putObject(request, RequestBody.fromFile(zipPath));
-	}
-
-	private static S3Client getS3Client(
-		String accessKey, String secretKey, String region
-	) {
-		return S3Client.builder()
-			.region(Region.of(region))
-			.credentialsProvider(
-				StaticCredentialsProvider.create(
-					AwsBasicCredentials.create(accessKey, secretKey)
-				)
-			)
-			.build();
 	}
 }
