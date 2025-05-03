243f851144db8f7b233c92788c2936516d9dea25
you12 <tjf7894@gmail.com>
2025-05-03 12:38:29 +0900

    Test: CommentValidationException (Green)

--- src/main/java/com/sprint/part3/sb01_monew_team6/exception/comment/CommentValidationException.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/exception/comment/CommentValidationException.java
@@ -1,5 +1,11 @@
 package com.sprint.part3.sb01_monew_team6.exception.comment;
 
-public class CommentValidationException {
+import com.sprint.part3.sb01_monew_team6.exception.ErrorCode;
+import java.time.Instant;
+import org.springframework.http.HttpStatus;
 
+public class CommentValidationException extends CommentException {
+  public CommentValidationException() {
+    super(ErrorCode.VALIDATION_ERROR, Instant.now(), HttpStatus.BAD_REQUEST);
+  }
 }
