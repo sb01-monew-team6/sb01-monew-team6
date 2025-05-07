7d66b30faf066ae908830c8cceb6889a195d8385
Son DongHyuk <sond5510@naver.com>
2025-05-07 20:11:10 +0900

    refactor : CommentComtrollerTest getComments_withValidParams_shouldReturnCommentList 메서드 페이지네이션 content 수정

--- src/main/java/com/sprint/part3/sb01_monew_team6/dto/PageResponse.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/dto/PageResponse.java
@@ -9,8 +9,8 @@ public record PageResponse<T>(
 		 * JSON 직렬화(serialization) 시엔 "content" 로,
 		 * 역직렬화(deserialization) 시엔 "content" 또는 "contents" 둘 다 허용
 		 */
-		@JsonProperty("content")
-		@JsonAlias("contents")
+		@JsonProperty("content") // JSON으로 나갈 때 “content” 라는 키를 사용
+		@JsonAlias("contents") // JSON으로 들어올 때 “contents” 도 허용
 		List<T> contents,
 		Object nextCursor,
 		Object nextAfter,
