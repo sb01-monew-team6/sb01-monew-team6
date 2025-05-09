c27776b110a056818f397300e631dd557562e140
Son DongHyuk <sond5510@naver.com>
2025-05-09 09:12:28 +0900

    refactor : ArticleViewController

--- src/main/java/com/sprint/part3/sb01_monew_team6/controller/ArticleViewController.java
+++ src/main/java/com/sprint/part3/sb01_monew_team6/controller/ArticleViewController.java
@@ -1,8 +1,12 @@
 package com.sprint.part3.sb01_monew_team6.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import com.sprint.part3.sb01_monew_team6.dto.news.ArticleViewDto;
 import com.sprint.part3.sb01_monew_team6.service.impl.ArticleViewServiceImpl;
+import javax.xml.transform.OutputKeys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -10,19 +14,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping({
-    "/api/articles/{articleId}/article-views",
-    "/articles/{articleId}/article-views",
-    "/articles/articles/{articleId}/article-views"
-})
+//@RequestMapping({
+//    "/api/articles/{articleId}/article-views",
+//    "/articles/{articleId}/article-views",
+//    "/articles/articles/{articleId}/article-views"
+//    "/api/articles"
+//})
+@RequestMapping("/api/articles")
 @RequiredArgsConstructor
 public class ArticleViewController {
 
   private final ArticleViewServiceImpl articleViewImplService;
 
-  @PostMapping
-  public ArticleViewDto viewArticle(
+  @PostMapping("/{articleId}/article-views")
+  public ResponseEntity<ArticleViewDto> viewArticle(
       @PathVariable Long articleId, @RequestHeader("Monew-Request-User-ID") Long userId){
-    return articleViewImplService.viewArticle(articleId, userId);
+    ArticleViewDto articleView = articleViewImplService.viewArticle(articleId, userId);
+    return ResponseEntity.status(OK).body(articleView);
   }
+
 }
