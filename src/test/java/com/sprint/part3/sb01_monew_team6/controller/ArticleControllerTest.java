1cf78411a721364c6d551258b72d4ae2145d4c08
Son DongHyuk <sond5510@naver.com>
2025-05-09 09:23:08 +0900

    refactor : ArticleViewControllerTest

--- src/test/java/com/sprint/part3/sb01_monew_team6/controller/ArticleControllerTest.java
+++ src/test/java/com/sprint/part3/sb01_monew_team6/controller/ArticleControllerTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
